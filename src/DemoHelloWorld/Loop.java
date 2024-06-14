132b364265b9083dbcc157eeef207ae6a1ac4702
Duc Toan <22120376@student.hcmus.edu.vn>
2024-06-14 17:21:55 +0700

    loop

---
+++ src/DemoHelloWorld/Loop.java
@@ -0,0 +1,45 @@
+package DemoHelloWorld;
+
+public class Loop {
+	public static void main(String[] args) {
+		// For
+		for (int i = 0; i < 5; i++) {
+			System.out.println(i);
+		}
+
+		// While
+		System.out.println("=============");
+		int i = 0;
+		while (i < 6) {
+			System.out.println(i);
+			i++;
+		}
+
+		// Do...while
+		System.out.println("=============");
+		int k = 1;
+		do {
+			System.out.println(k);
+			k++;
+		} while (k < 5);
+
+		// Break
+		System.out.println("============");
+		for (int j = 0; j < 5; j++) {
+			System.out.println(j);
+			if (j == 3) {
+				break; // thoat khoi vong lap
+			}
+		}
+
+		// Continue
+		System.out.println("============");
+		for (int j = 0; j < 5; j++) {
+			if (j == 3) {
+				continue; // thoat khoi lan lap
+			}
+			System.out.println(j);
+
+		}
+	}
+}
