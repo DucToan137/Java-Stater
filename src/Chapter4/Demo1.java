c4bdb80b90c0ab6e3346a595e05b797ea3ae1fbc
Duc Toan <22120376@student.hcmus.edu.vn>
2024-07-10 00:54:48 +0700

    Dinh nghia Constructor

---
+++ src/Chapter4/Demo1.java
@@ -0,0 +1,14 @@
+package Chapter4;
+
+public class Demo1 {
+	public static void main(String[] args) {
+		System.out.println("Run demo1");
+
+		Student st1 = new Student();
+
+		Student st2 = new Student("Eric", 25);
+
+		System.out.println("check obj: " + st1._name + " age = " + st1._age);
+		System.out.println("check obj: " + st2._name + " age = " + st2._age);
+	}
+}
