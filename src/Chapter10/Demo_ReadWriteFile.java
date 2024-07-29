331c972a167ab6857d4666ce3ed0e80f15c51271
Duc Toan <22120376@student.hcmus.edu.vn>
2024-07-29 16:16:28 +0700

    Demo Read/Write File

---
+++ src/Chapter10/Demo_ReadWriteFile.java
@@ -0,0 +1,31 @@
+package Chapter10;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Demo_ReadWriteFile {
+	public static void main(String[] args) throws IOException {
+
+		FileInputStream in = null;
+		FileOutputStream out = null;
+
+		try {
+			in = new FileInputStream("./src/Chapter10/input.txt");
+			out = new FileOutputStream("outagain.txt");
+			int c;
+
+			while ((c = in.read()) != -1) {
+				out.write(c);
+			}
+		} finally {
+			if (in != null) {
+				in.close();
+			}
+			if (out != null) {
+				out.close();
+			}
+			System.out.println("END");
+		}
+	}
+}
